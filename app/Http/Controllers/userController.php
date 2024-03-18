2a53074b1f765c94ece484d41d27bd4d176a488b
UKM Seni Theatrisic <113005391+iannstronaut@users.noreply.github.com>
2024-03-18 23:54:29 +0700

    test: isClean, isDirty, dan wasChanged

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -35,13 +35,38 @@ class userController extends Controller
             // $user = UserModel::where('level_id', 2)->count();
             // // dd($user);
         $user = UserModel::firstOrNew([
-            'username' => 'manager33',
-            'nama' => 'Manager Tiga Tiga',
+            'username' => 'manager11',
+            'nama' => 'Manager11',
             'password' => Hash::make('12345'),
             'level_id' => 2
         ]);
+        $user->username = 'manager 12';
 
         $user->save();
-        return view('user', ['data' => $user]);
+
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged(['username','level_id']);
+        $user->wasChanged('nama');
+        $user->wasChanged(['nama', 'username']);
+        dd($user->wasChanged(['nama', 'username']));
+
+        // $user->isDirty();
+        // $user->isDirty('username');
+        // $user->isDirty('nama');
+        // $user->isDirty(['nama', 'username']);
+
+        // $user->isClean();
+        // $user->isClean('username');
+        // $user->isClean('nama');
+        // $user->isClean(['nama','username']);
+
+        // $user->save();
+
+        // $user->isDirty();
+        // $user->isClean();
+
+        // dd($user->isDirty());
+        // return view('user', ['data' => $user]);
     }
 }

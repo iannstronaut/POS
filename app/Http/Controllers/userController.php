fac31e822f338430ee43eae4b152ed7839139074
UKM Seni Theatrisic <163162130+Theatrisic@users.noreply.github.com>
2024-03-18 20:06:03 +0700

    test: fillabel field

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -16,20 +16,21 @@ class userController extends Controller
     }
 
     public function index(){
-        // $data =[
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        // ];
-        // UserModel::insert($data);
-
-        $data = [
-            'nama' => 'Pelanggan Pertama'
+        $data =[
+            'level_id' => 2,
+            'username' => 'manager-tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data);
+        UserModel::create($data);
 
         $user = UserModel::all();
         return view('user', ['data' => $user]);
+
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama'
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data);
+
     }
 }
